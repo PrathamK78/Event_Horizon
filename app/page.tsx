16b4ab28c707a04fc444e291a63da30cd7bd97ed
Pratham Kapoor <prathamkapoor1906@gmail.com>
2025-12-02 22:16:34 +0530

    First Commit

---
+++ app/page.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import ExploreBtn from "@/components/ExploreBtn";
+import EventCard from "@/components/EventCard";
+import { events } from "@/lib/constants";
+
+const Page = () => {
+    return (
+        <section>
+            <h1 className='text-center'>The Hub for every dev <br/>Event you can't miss</h1>
+            <p className='text-center mt-5'>Hackathons, Meetups, Conferences, All in one place.</p>
+            <ExploreBtn/>
+
+            <div className='mt-20 space-y-7'>
+                <h3>Featured Events</h3>
+
+                <ul className='events'>
+                    {events.map((event) => (
+                        <li key={event.title}>
+                            <EventCard { ... event} />
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </section>
+    )
+}
+export default Page
